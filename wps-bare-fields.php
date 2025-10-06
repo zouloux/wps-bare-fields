2fa194e92f6d63135239aa4730643250ac97f41e
Alexis Bouhet <zouloux@gmail.com>
2025-10-06 15:54:56 +0200

    cleanRepeat style

--- wps-bare-fields.php
+++ wps-bare-fields.php
@@ -10,7 +10,7 @@
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       Bare Fields
  * Domain Path:       /cms
- * Version:           0.10.1
+ * Version:           0.10.2
  * Copyright:         © 2024 Alexis Bouhet
  */
 
