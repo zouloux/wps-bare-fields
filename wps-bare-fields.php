a02a6a1268a0846fa6a349e63eb8a40bfa9461fe
Alexis Bouhet <zouloux@gmail.com>
2026-08-20 10:34:32 +0200

    Admin color WP 7.0 fixed

--- wps-bare-fields.php
+++ wps-bare-fields.php
@@ -10,7 +10,7 @@
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       Bare Fields
  * Domain Path:       /cms
- * Version:           0.12.5
+ * Version:           0.12.6
  * Copyright:         © 2024 Alexis Bouhet
  */
 
